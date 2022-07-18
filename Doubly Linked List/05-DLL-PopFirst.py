ff37651e99113762b64cde13788700049b376589
aryan690 <aryansetiaa@gmail.com>
2022-07-18 16:19:34 +0530

    initial

--- Doubly Linked List/05-DLL-PopFirst.py
+++ Doubly Linked List/05-DLL-PopFirst.py
@@ -47,16 +47,40 @@ class DoublyLinkedList:
 		self.length -=1
 		return temp.value
 
+	def prepend(self, value):
+		new_node = Node(value)	
+		if self.head is None:
+			self.head = new_node 
+			self.tail = new_node 
+		else:
+			new_node.next = self.head 
+			self.head.prev = new_node 
+			self.head = new_node
+		self.length += 1
+		return True
 
 
-		
+	def pop_first(self):
+		if self.length == 0:
+			return None
+		temp = self.head 
+		if self.length == 1:
+			self.head = None 
+			self.tail = None 
+		else:
+			self.head = self.head.next 
+			self.head.prev = None 
+			temp.next = None 
+		self.length -= 1
+		return temp.value
 
+			
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 my_doubly_linked_list.append(3)
-my_doubly_linked_list.pop()
-my_doubly_linked_list.pop()
-my_doubly_linked_list.pop()
+my_doubly_linked_list.pop_first()
+my_doubly_linked_list.pop_first()
+my_doubly_linked_list.pop_first()
 
 
 my_doubly_linked_list.print_list()
--- Doubly Linked List/06-DLL-Get.py
+++ Doubly Linked List/06-DLL-Get.py
@@ -47,16 +47,54 @@ class DoublyLinkedList:
 		self.length -=1
 		return temp.value
 
+	def prepend(self, value):
+		new_node = Node(value)	
+		if self.head is None:
+			self.head = new_node 
+			self.tail = new_node 
+		else:
+			new_node.next = self.head 
+			self.head.prev = new_node 
+			self.head = new_node
+		self.length += 1
+		return True
 
 
-		
+	def pop_first(self):
+		if self.length == 0:
+			return None
+		temp = self.head 
+		if self.length == 1:
+			self.head = None 
+			self.tail = None 
+		else:
+			self.head = self.head.next 
+			self.head.prev = None 
+			temp.next = None 
+		self.length -= 1
+		return temp.value
 
+
+	def get(self, index):
+		if index < 0 or index >= self.length:
+			return None 
+		temp = self.head
+		if index < self.length/2: 
+			for _ in range(index):
+				temp = temp.next 
+		else:
+			temp = self.tail
+			for _ in range(self.length-1, index, -1):
+				temp = temp.prev
+		return temp.value 
+
+
+			
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 my_doubly_linked_list.append(3)
-my_doubly_linked_list.pop()
-my_doubly_linked_list.pop()
-my_doubly_linked_list.pop()
+my_doubly_linked_list.append(4)
 
+print(my_doubly_linked_list.get(1))
 
-my_doubly_linked_list.print_list()
+# my_doubly_linked_list.print_list()
