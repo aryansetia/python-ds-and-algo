65caa7fc35a06f942508087e4cd95464c7c54b24
aryan690 <aryansetiaa@gmail.com>
2022-07-24 20:23:19 +0530

    selection sort

---
+++ Sorting/Selection Sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+	for i in range(len(arr)-1): #for(int i=0; i<n-1; i++)
+		min_index = i
+		for j in range(i+1, len(arr)):
+		  	if arr[j] < arr[min_index]:
+		  		min_index = j
+		arr[i], arr[min_index] = arr[min_index], arr[i]
+
+	return arr
+
+
+print(selection_sort([4,2,6,5,1,3]))		
+
